4157850539e784eb955c99d391860829268ec2b5
Ashish Oraon <ashish.oraon@ect-telecoms.de>
2023-03-29 13:32:32 +0200

    fetching from google sheet

--- googleSheet.js
+++ googleSheet.js
@@ -1,27 +1,36 @@
-const sheetId = '2PACX-1vQDZUgLCtmyyQulx9km_rH9glT4PV6jQZFLnjhW_dPl-V-jAUkoG4JJ1D_I8gJXIwPSIKAxEleSs5s6';
-const sheetName = 'Sheet1';
+const sheetId = "1tRdogZyQuA4kBA6UhKLDwOTaOkAi2-gsrmSkFGuZxKI";
+const sheetName = "Sheet1";
 
 // Step 1: Fetch the data from Google Sheets API
-fetch(`https://spreadsheets.google.com/feeds/list/${sheetId}/od6/public/values?alt=json`)
-  .then(response => response.json())
-  .then(data => {
+// fetch(`https://spreadsheets.google.com/feeds/list/${sheetId}/od6/public/values?alt=json`)
+fetch(
+  `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?sheet=${sheetName}`
+  // `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?sheet=${sheetName}&range=${sheetRange}`
+)
+  .then((res) => res.text())
+  .then((data) => {
     // Step 2: Extract the relevant data from the response
-    const rows = data.feed.entry;
-    const headers = Object.keys(rows[0]).filter(header => header.startsWith('gsx$'));
-    
-    // Step 3: Convert the data to a JSON object
-    const items = rows.map(row => {
-      const item = {};
-      headers.forEach(header => {
-        const key = header.replace('gsx$', '');
-        item[key] = row[header].$t;
+    let rep = JSON.parse(data.substr(47).slice(0, -2));
+    const table = rep.table;
+    const [headersRaw, ...tableDataRaw] = table.rows;
+    const headers = headersRaw.c.map((col) => col.v);
+    const tableData = tableDataRaw.map((t) => t.c.map((col) => col.v));
+
+    const iFrames = tableData
+      .filter((f) => f[0].startsWith("<iframe "))
+      .map((el) => {
+        const frameDiv = document.createElement("div");
+        frameDiv.innerHTML = el[0];
+        return frameDiv;
       });
-      return item;
-    });
-    console.log('fetching google sheet');
-    // Step 4: Do something with the JSON object (e.g. log it to console)
-    console.log(items);
+
+    const listDiv = document.getElementById("productList");
+    if (listDiv) {
+      iFrames.forEach((frame) => {
+        listDiv.appendChild(frame);
+      });
+    }
   })
-  .catch(error => {
-    console.error('Error fetching data:', error);
+  .catch((error) => {
+    console.error("Error fetching data:", error);
   });
